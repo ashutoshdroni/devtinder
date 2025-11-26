48c809650b6671f07e315661ba9940cb77f077e3
ashutoshdronni <droniashutosh2@gmail.com>
2025-11-26 21:16:57 +0530

    second line of the commit message.

--- src/app.js
+++ src/app.js
@@ -2,17 +2,37 @@ const express = require("express");
 const app = express();
 
 app.use("/support",(req,res)=>{
-    res.send("Hello ashu");
+    res.send("hell bro");
 });
-app.use("/home",(req,res)=>{
-    res.send("Hello bro");
+
+app.get("/home",(req,res)=>{
+    res.send({firstname:"John",lastname:"pagla"});
 });
-app.use("/test",(req,res)=>{
+app.get("/ab\\+c",(req,res)=>{
+    res.send({firstname:"John",lastname:"kumar"});
+});
+app.get("/ab\\?c",(req,res)=>{
+    res.send({firstname:"John",lastname:"singh"});
+});
+app.get("/ab\\*cd",(req,res)=>{
+    res.send({firstname:"John",lastname:"namaste"});
+});
+app.get("/a\\(bc\\)\\?d",(req,res)=>{
+    res.send({firstname:"John",lastname:"bhai"});
+});
+
+
+
+app.post("/test",(req,res)=>{
     res.send("Hello test");
 });
-app.use("/",(req,res)=>{
-    res.send("Hello World");
+app.delete("/",(req,res)=>{
+    res.send("deleted all files");
 });
+app.use((req, res) => {
+  res.status(404).send({ error: "Route not found" });
+});
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 });
