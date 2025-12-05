39cd32180ca0ef8318cbb8569d042a3e8ab34582
ashutoshdronni <droniashutosh2@gmail.com>
2025-12-05 18:11:33 +0530

    commit 3

--- src/models/user.js
+++ src/models/user.js
@@ -1,18 +1,53 @@
-const mongoees = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
-const userSchema = new mongoees.Schema({
+const userSchema = new mongoose.Schema({
+    
     firstName:{
-        type:String
+        
+        type:String,
+        required:true,
+        minlength:8,
+        maxlength:20,
     },
     lastName:{
-        type:String
+        type:String,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    age:{
+        type:Number,
+        min:18,
+        max:65,
+    },
+    gender:{
+        type:String,
+        validate(value){
+            if(!["male",female,"other"].includes(value)){
+                throw new Error("Gender must be male,female or other");
+            }
+        }
     },
     email:{
         type:String,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is invalid"+value);
+            }
+        }       
+
+
     },
     password:{
         type:String,
-    }
-});
-const User = mongoees.model("User",userSchema);
-module.exports=User;    
+    },
+    
+},
+
+{  
+    timestamps:true,
+}
+);
+const user = mongoose.model("User",userSchema);
+module.exports=user;    
