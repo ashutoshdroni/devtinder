e19554d31483b42e36b17e21733bc68c1e96ddac
ashutoshdronni <droniashutosh2@gmail.com>
2025-12-05 18:44:35 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -4,19 +4,38 @@ const app = express();
 const User =require("./models/user");
 app.use(express.json());
 const validateSignupData = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 
 app.post("/signup",async (req,res) => {
-    
     try{
         validateSignupData(req);
-        const user = new User(req.body);
+        const {firstName,lastName,email,password} = req.body;
+        const hashedPassword = await bcrypt.hash(password,10);
+        const user = new User(firstName,lastName,email,password);
         await user.save();
         res.send("User added successfully");
     }catch(err){
         res.status(400).send(err);
     }
 });
+app.post("/login",async (req,res) => {
+    try{
+        const {email,password} = req.body;
+        const user = await User.findOne({email:email});
+        if(!user){
+            throw new Error("User not found");
+        }
+        const isPasswordMatch = await bcrypt.compare(password,user.password);
+        if(!isPasswordMatch){
+            throw new Error("Invalid password");
+        }else{
+            res.send("Login successful");
+        }
+    }catch(err){
+        res.status(400).send
+    }
+});               
 app.get("/feed",async (req,res) => {
     try{
         const users = await User.find({});
